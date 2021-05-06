9704a66034c8fc119e2d780600c2436bd62e35ac
Andrea Nájera <a25526@svalero.com>
2021-05-06 09:00:59 +0200

    opciones

--- src/main/java/com/sanvalero/application/Domotica.java
+++ src/main/java/com/sanvalero/application/Domotica.java
@@ -8,6 +8,28 @@ public class Domotica {
         System.out.println("Elige una opcion: (lavadoraOn ,lavadoraOff, persianaUp, persianaDown , lucesOn, lucesOff) ");
         String eleccion = entrada.next();
 
-
-
+        switch(eleccion){
+            case"lavadoraOn":
+                encenderLavadora();
+                break;
+            case"lavadoraOff":
+                apagarLavadora();
+                break;
+            case"persianaOn":
+                levantarPersiana();
+                break;
+            case"persianaOff":
+                bajarPersiana();
+                break;
+            case"lucesOn":
+                encenderLuces();
+                break;
+            case"lucesOff":
+                apagarLuces();
+                break;
+            default:
+                System.out.println("No has elegido una opcion correcta");
+                break;
+        }
+    }
 }
